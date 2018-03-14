2e7da8ffc6a5901b81c33f4405ef0317352842f2
InjeongChoii <starec6230@kookmin.ac.kr>
2018-03-15 01:05:37 +0900

    Add IndexActivity for Receiving result

---
+++ src/Android_App/app/src/main/java/capstone2018/coway/SuccessResultActivity.java
@@ -0,0 +1,13 @@
+package capstone2018.coway;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SuccessResultActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_success_result);
+    }
+}
