85ad26584f4c4459105374f41e2cf044f4953117
InjeongChoii <starec6230@kookmin.ac.kr>
2018-03-18 17:03:01 +0900

    Apply Packet libs to proj for Socket io

---
+++ src/Android_App/app/src/main/java/capstone2018/coway/LogicalErrorActivity.java
@@ -0,0 +1,13 @@
+package capstone2018.coway;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LogicalErrorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_logical_error);
+    }
+}
