76be04de9348b3f7354776a7f1dbb9dac896fe24
신종혁 <shinjong9312@gmail.com>
2018-03-18 13:30:25 +0900

    IP_3 issue clear -contour making-

--- src/imageProcessing/src/main.py
+++ src/imageProcessing/src/main.py
@@ -12,17 +12,17 @@ import os
 파일 불러오기 및 영상 처리
 """
 def process():
-	"""
-	파일 불러오기 및 히스토그램 균일화
-	"""
+
+	#파일 불러오기 및 히스토그램 균일화
+
 	path = "../images/*.jpg"
-	img = cv2.imread('../images/pseudo2.jpg', cv2.IMREAD_GRAYSCALE)
+	img = cv2.imread('../images/clear.jpg', cv2.IMREAD_GRAYSCALE)
 	clahe = cv2.createCLAHE(clipLimit = 2.0, tileGridSize = (8,8))
 	img2 = clahe.apply(img)
 
-	"""
-	퓨리에 변환 실시 
-	"""
+
+	#퓨리에 변환 실시
+
 	f = np.fft.fft2(img2)
 	fshift = np.fft.fftshift(f)
 	rows , cols = img.shape
@@ -35,28 +35,23 @@ def process():
 	f_ishift = np.fft.ifftshift(fshift)
 	img_back = np.fft.ifft2(f_ishift)
 	img_back = np.abs(img_back)
-	"""
-	임계값 설정
-	"""	
+
+	#임계값 설정
+
 	thresholder = 55
 	whiteValue = 255
 
-	"""
-	블러 이미지 생성 및 임계값 적용, 최종 이미지 저장
-	"""
+
+	#블러 이미지 생성 및 임계값 적용, 최종 이미지 저장
+
 	blur = cv2.GaussianBlur(img_back,(5,5),0)
 
 	ret1,finalImg = cv2.threshold(blur,thresholder,whiteValue,cv2.THRESH_BINARY)
 
-	cv2.imwrite('../images/ss.png',finalImg)
+	cv2.imwrite('../images/ss.jpg',finalImg)
 
-	"""
-	테저렉트 실행(인식 불량)
-	"""
-	text = pyt.image_to_string(Image.open('../images/ss.png'))
 
-	print(text)
-
+    #테저렉트 실행(인식 불량)
 
 process()
 
