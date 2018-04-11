3ad30858f1ea631b4f2d943b35985f773faa1f6f
신종혁 <shinjong9312@gmail.com>
2018-04-11 20:35:35 +0900

    IP_7 get argv for setting path.

--- src/imageProcessing/src/tesseract.py
+++ src/imageProcessing/src/tesseract.py
@@ -1,13 +1,14 @@
 from pytesseract import *
-import cv2
 from PIL import Image
+import sys
 
-def OCR(img, lang='eng'):
+def OCR(img, path='result',lang='eng'):
     im = Image.open(img)
     text = image_to_string(im, lang = lang)
-    f = open('../result/result.txt','w')
+    f = open('../' + path + '/result.txt','w')
     f.write(text)
 
 
 if __name__ == '__main__':
-    OCR('../images/text.png')
+    PATH = sys.argv[1]
+    OCR('../images/text.png',PATH)
