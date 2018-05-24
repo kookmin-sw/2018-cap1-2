6cca8187b98e223b46d1a269ea10fde9f9088111
S.J. Lee <occidere@naver.com>
2018-05-25 01:26:19 +0900

    Coway 0.0.0.7 배포

--- src/Interpreter/Converted/converted.java
+++ src/Interpreter/Converted/converted.java
@@ -1,33 +1,10 @@
-for(int i=0;i<=N;i++){
-System.out.print(i);
-System.out.println("값:"+i);
+int a = 0;
+int b = 1;
+int k = 100;
+if (a < k){
+for(int n=0;n<=10;n++){
+a = a+b;
+b=a;
+
 }
-
-//신규변수_매칭_테스트
-int a = 10;
-char b = '!';
-String c = "Hello World!";
-double d = 3.14;
-TestClass e = new TestClass();
-
-//기존변수_재정의_테스트
-a = 0;
-b = '?';
-c = "Hell World!";
-d = 3.141592;
-e = new TestClass();
-
-//신규변수_매칭_테스트
-int id = 20123087;
-char operator = '+';
-String tt = "ᕙ(•̀‸•́‶)ᕗ";
-double kill = .1;
-Twice twice = new Twice();
-
-int i = 0;
-int j = 10;
-for(int k=0;k<=N;k++){
-i = i + j;
-j = i;
-line = br.readLine();
 }
